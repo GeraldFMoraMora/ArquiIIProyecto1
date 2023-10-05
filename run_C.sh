bfd3cc03f8eb93c3d2ff4a5230c7c16465d7c742
geraldfmoramora <geraldmoramora@gmail.com>
2023-10-05 00:13:44 -0600

    Agregado scripts para autoejecucion y lectura de resultados

---
+++ run_C.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Lista de programas a compilar y ejecutar
+programas=("mSSTC" "mSFSC" "mSNFS1C" "mSNFS2C")
+
+# Elimina los archivos de resultados existentes (si los hay)
+for programa in "${programas[@]}"; do
+    if [ -e "${programa}C_results.txt" ]; then
+        echo "Eliminando archivo de resultados existente para $programa"
+        rm "${programa}C_results.txt"
+    fi
+    if [ -e "${programa}" ]; then
+        echo "Eliminando archivo de compilacion existente para $programa"
+        rm "${programa}"
+    fi
+done
+
+# Compila y ejecuta todos los programas
+for programa in "${programas[@]}"; do
+    echo "Compilando $programa..."
+    gcc -o "$programa" "${programa}.c" -lpthread
+
+    # Verifica si la compilación fue exitosa
+    if [ $? -eq 0 ]; then
+        valgrind --tool=cachegrind "./$programa" 2> "${programa}C_results.txt"
+        
+    else
+        echo "Error: La compilación de $programa ha fallado."
+    fi
+done
---
+++ run_CPP.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Lista de programas a compilar y ejecutar
+programas=("mSST" "mSFS" "mSNFS1" "mSNFS2")
+
+# Elimina los archivos de resultados existentes (si los hay)
+for programa in "${programas[@]}"; do
+    if [ -e "${programa}CPP_results.txt" ]; then
+        echo "Eliminando archivo de resultados existente para $programa"
+        rm "${programa}CPP_results.txt"
+    fi
+    if [ -e "${programa}" ]; then
+        echo "Eliminando archivo de compilacion existente para $programa"
+        rm "${programa}"
+    fi
+done
+
+# Compila y ejecuta todos los programas
+for programa in "${programas[@]}"; do
+    echo "Compilando $programa..."
+    g++ -o "$programa" "${programa}.cpp" -pthread -std=c++11 -lbenchmark
+
+    # Verifica si la compilación fue exitosa
+    if [ $? -eq 0 ]; then
+        echo "Ejecutando Valgrind con Cachegrind para $programa..."
+        valgrind --tool=cachegrind "./$programa" 2> "${programa}CPP_results.txt"
+        
+    else
+        echo "Error: La compilación de $programa ha fallado."
+    fi
+done
